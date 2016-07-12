78536f259b6aca57b3bf1e371c59f35a1def82bc
chenluojie <chen.rojer@gmail.com>
2016-07-12 09:44:46 +0800

    commit

--- src/components/part2.vue
+++ src/components/part2.vue
@@ -3,22 +3,17 @@
 <p>当前路径: {{$route.path}}</p>
   <p>当前路由参数: {{$route.params | json}}</p>
    <p>当前查询参数: {{$route.query | json}}</p>
-   <p>field: {{field1}}</p>
+   <p>field: {{temp}}</p>
 <router-view></router-view>
 </template>
 
 <script>
     export default {
         data: function() {
-            return new Promise(function(resolve, reject) {
-                resolve();
-            }).then(function() {
-                console.log(this.$route.path);
-                return {
-                    part1temp: 'Hello Vue, Hello world'
-                };
-            });
+            return {
+                msg: 'Hello world',
+                temp: '/foo/baz/tttt'
+            };
         }
-
     }
 </script>
